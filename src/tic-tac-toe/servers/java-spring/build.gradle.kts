32a6bf63a5f45db6c21e92fbf84b730fba1cf759
Ahmed Nadeem <ahmed210899@gmail.com>
2024-02-04 21:18:36 +0500

    Setup dependencies

--- src/tic-tac-toe/servers/java-spring/build.gradle.kts
+++ src/tic-tac-toe/servers/java-spring/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "org.solutioneers"
@@ -10,8 +12,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
